d103252089b27056b21f26ee66d20dd7e10e94c8
Marco Hernandez <marco.hernandez@globant.com>
2017-03-07 06:10:14 -0500

    migrations and final migration sent

--- sql/tables/migrations.sql
+++ sql/tables/migrations.sql
@@ -3,5 +3,9 @@ DROP TABLE IF EXISTS migrations;
 CREATE TABLE migrations(
 	id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	created DATETIME NOT NULL,
-	group_id INT NOT NULL DEFAULT 0
+	group_id INT NOT NULL DEFAULT 0,
+	description VARCHAR(256) NOT NULL,
+
+	INDEX ix_migrations_group_id (group_id DESC)
 );
+
