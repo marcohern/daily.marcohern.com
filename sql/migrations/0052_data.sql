7d9344b774a168ff0ab1dd5f51f41003d89ec19c
Marco Hernandez <marcohern@gmail.com>
2017-05-11 23:00:33 -0500

    fixed script

--- sql/migrations/0052_data.sql
+++ sql/migrations/0052_data.sql
@@ -8,7 +8,7 @@ SET event_date = '2015-05-11 07:40:00',
 WHERE event_date = '2015-05-10 07:40:00';
 
 UPDATE trans SET group_id = 51
-WHERE event_date >= '2015-05-10 08:24:16';
+WHERE event_date >= '2017-05-10 08:24:16';
 
 -- Trans
 INSERT INTO trans (event_date, value, mode, group_id, category_id, created) VALUES
@@ -19,3 +19,7 @@ INSERT INTO trans (event_date, value, mode, group_id, category_id, created) VALU
 INSERT INTO syncs (event_date, group_id, processed, bank, pockets, storage, pr_bank, pr_pockets, pr_storage, created) VALUES
 ('2017-05-11 22:53:00',51,'false',8231579.34,220800,0,8231579.34,226200,0,NOW());
 
+UPDATE syncs
+SET event_date = '2017-05-11 07:40:00',
+	group_id = 51
+WHERE event_date = '2015-05-11 07:40:00';
